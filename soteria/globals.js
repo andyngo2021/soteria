e9c68d70530723a5a4d1bdc3b5d580ce9a04329e
Jaggerafk <62218530+Jaggerafk@users.noreply.github.com>
2021-01-07 18:06:14 -0500

    Add files via upload

---
+++ soteria/globals.js
@@ -0,0 +1,16 @@
+// London's Longitude and Latitude 
+let _lng = 0.1278;
+let _lat = 51.5074;
+
+// UC Berkeley's Lng/Lat
+let b_lng  = -122.2585; 
+let b_lat = 37.8719;
+
+let all_crimes = [];
+let all_restrooms = [];
+let all_refuges = [];
+
+/*
+
+
+*/
