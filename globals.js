e422ace2832dc806713f3a8db9ed2e030fc889a1
Andy Ngo <andyngo2021@gmail.com>
2021-01-07 08:44:36 -0800

    IM JUMP BRDIGE

---
+++ globals.js
@@ -0,0 +1,7 @@
+// London's Longitude and Latitude 
+let _lng = 0.1278;
+let _lat = 51.5074;
+
+// UC Berkeley's Lng/Lat
+let b_lng  = -122.2585; 
+let b_lat = 37.8719;
--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-let SF_lng = -122.4194;
-let SF_lat = 37.7749;
-let b_lng  = -122.2585; 
-let b_lat = 37.8719;
+// API call example
+async function goGetData()
+{
+    let resp = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await resp.json();
+    console.log(data);
+}
 
+// API call to REFUGE to get restrooms
+async function getRestrooms(lat, lng)
+{
+    let url = 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?lat='+lat+'&lng='+lng;
+    let resp = await fetch(url);
+    let data = await resp.json();
+    console.log(data);
+}
 
+// let all_crimes = [];
+// // Use this function to call on getCrime to get all 12 months of 2020 crime
+// function getAllCrimes(lat, lng)
+// {
+//     for (let i=1; i<=12; i++)
+//     {
+//         getCrime(_lat, _lng, i.toString());
+//     }
+//     console.log(all_crimes.length);
+//     console.log(all_crimes);
+// }
+
+// API call to BikeWise to get reported thefts
+async function getCrime(lat, lng, month) // All strings btw
+{
+    let url = 'https://data.police.uk/api/crimes-at-location?date=2020-'+month+'&lat='+lat+'&lng='+lng
+    let resp = await fetch(url);
+    let data = await resp.json();
+    console.log(data);
+}
 
 // Actual Map Object
 // https://openlayers.org/en/latest/doc/quickstart.html 
@@ -15,7 +46,7 @@ var map = new ol.Map({
         })
     ],
     view: new ol.View({
-        center: ol.proj.transform([SF_lng, SF_lat], 'EPSG:4326', 'EPSG:3857'),
+        center: ol.proj.transform([_lng, _lat], 'EPSG:4326', 'EPSG:3857'),
         zoom: 10
     })
 });
